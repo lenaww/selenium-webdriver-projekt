013f39dae241f43a6e931ebce95b78e1f2c9db8c
lena <lenaw2222@gmail.com>
2021-06-13 15:54:37 +0000

    lokalizatory

---
+++ projekt_lokalizatory.py
@@ -0,0 +1,34 @@
+# Akceptacja cookies
+xpath:'//*[@id="onetrust-accept-btn-handler"]'
+        
+# LOGOWANIE
+xpath:'/html/body/div[1]/header/div/div[2]/div[2]/div[3]/a'
+        
+# REJESTRACJA
+css_selector:'a[data-registration-url="/customer/register"]'
+
+# DANE DO FORMULARZA:
+# imię:
+xpath:/html/body/div[1]/div[2]/div/div[1]/div[3]/span/div[1]/div[1]/span/input
+# nazwisko
+xpath:/html/body/div[1]/div[2]/div/div[1]/div[3]/span/div[1]/div[2]/span/input    
+# niepoprawny email
+xpath:/html/body/div[1]/div[2]/div/div[1]/div[3]/span/div[1]/div[3]/span/input
+# hasło
+xpath:/html/body/div[1]/div[2]/div/div[1]/div[3]/span/div[1]/div[4]/span/input
+# numer telefonu
+xpath:/html/body/div[1]/div[2]/div/div[1]/div[3]/span/div[1]/div[5]/span/input
+# Przewijanie strony         
+xpath://*[@id="page-register-form"]/div[1]/div[3]/div[3]/div/button
+# kobieta
+xpath://*[@id="female"] 
+# mezczyzna                
+xpath://*[@id="male"]
+# zgoda 1
+xpath://*[@id="page-register-form"]/div[1]/div[3]/span/div[3]/div[5]/span/div/label/span[3]
+# zgoda 2
+xpath://*[@id="page-register-form"]/div[1]/div[3]/span/div[3]/div[6]/span/div/label/span[3]
+# ZAKADANIE KONTA
+xpath://*[@id="page-register-form"]/div[1]/div[3]/div[3]/div/button
+# błędy
+xpath:/html/body/div[1]/div[2]/div/div[1]/div[3]/span/div[1]/div[3]/span/span
